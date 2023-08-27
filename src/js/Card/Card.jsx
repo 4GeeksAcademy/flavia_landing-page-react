a87d7d0eb43cd8eab73e15c2b874372f2793c820
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-27 17:22:52 +0000

    Componente Card listo

--- src/js/Card/Card.jsx
+++ src/js/Card/Card.jsx
@@ -1,52 +1,59 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 
-//create your first component
 const MyCard = () => {
 	return (
-		<CardGroup>
-		<Card>
-		  <Card.Img variant="top" src="holder.js/100px160" />
-		  <Card.Body>
-			<Card.Title>Card title</Card.Title>
-			<Card.Text>
-			  This is a wider card with supporting text below as a natural lead-in
-			  to additional content. This content is a little bit longer.
-			</Card.Text>
-		  </Card.Body>
-		  <Card.Footer>
-			<small className="text-muted">Last updated 3 mins ago</small>
-		  </Card.Footer>
-		</Card>
-		<Card>
-		  <Card.Img variant="top" src="holder.js/100px160" />
-		  <Card.Body>
-			<Card.Title>Card title</Card.Title>
-			<Card.Text>
-			  This card has supporting text below as a natural lead-in to
-			  additional content.{' '}
-			</Card.Text>
-		  </Card.Body>
-		  <Card.Footer>
-			<small className="text-muted">Last updated 3 mins ago</small>
-		  </Card.Footer>
-		</Card>
-		<Card>
-		  <Card.Img variant="top" src="holder.js/100px160" />
-		  <Card.Body>
-			<Card.Title>Card title</Card.Title>
-			<Card.Text>
-			  This is a wider card with supporting text below as a natural lead-in
-			  to additional content. This card has even longer content than the
-			  first to show that equal height action.
-			</Card.Text>
-		  </Card.Body>
-		  <Card.Footer>
-			<small className="text-muted">Last updated 3 mins ago</small>
-		  </Card.Footer>
-		</Card>
-	  </CardGroup>
+		<div className="d-flex flex-row">
+			<Card style={{ width: '18rem' }} className="me-3 text-center">
+				<Card.Img variant="top" src="https://via.placeholder.com/500x325" />
+				<Card.Body>
+					<Card.Title className="pt-2">Card Title</Card.Title>
+					<Card.Text className="pb-5">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Simimet sed non voluptatum esse commodi sapiente quam obcaecati consequatur.
+					</Card.Text>
+				</Card.Body>
+				<div className="border p-2 bg-secondary-subtle">
+					<Button variant="primary">Go somewhere</Button>
+				</div>
+    		</Card>
+			<Card style={{ width: '18rem' }} className="me-3 text-center">
+				<Card.Img variant="top" src="https://via.placeholder.com/500x325" />
+				<Card.Body>
+					<Card.Title className="pt-2">Card Title</Card.Title>
+					<Card.Text className="pb-5">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Simimet sed non voluptatum esse commodi sapiente quam obcaecati consequatur.
+					</Card.Text>
+				</Card.Body>
+				<div className="border p-2 bg-secondary-subtle">
+					<Button variant="primary">Go somewhere</Button>
+				</div>
+    		</Card>
+			<Card style={{ width: '18rem' }} className="me-3 text-center">
+				<Card.Img variant="top" src="https://via.placeholder.com/500x325" />
+				<Card.Body>
+					<Card.Title className="pt-2">Card Title</Card.Title>
+					<Card.Text className="pb-5">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Simimet sed non voluptatum esse commodi sapiente quam obcaecati consequatur.
+					</Card.Text>
+				</Card.Body>
+				<div className="border p-2 bg-secondary-subtle">
+					<Button variant="primary">Go somewhere</Button>
+				</div>
+    		</Card>
+			<Card style={{ width: '18rem' }} className="text-center">
+				<Card.Img variant="top" src="https://via.placeholder.com/500x325" />
+				<Card.Body>
+					<Card.Title className="pt-2">Card Title</Card.Title>
+					<Card.Text className="pb-5">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Simimet sed non voluptatum esse commodi sapiente quam obcaecati consequatur.
+					</Card.Text>
+				</Card.Body>
+				<div className="border p-2 bg-secondary-subtle">
+					<Button variant="primary">Go somewhere</Button>
+				</div>
+    		</Card>
+		</div>
 	)
 };
 
