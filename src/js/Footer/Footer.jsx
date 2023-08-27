34bc6930ed2b781696c54ecfffd1b27336b41681
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-27 17:31:51 +0000

    Component Footer listo

--- src/js/Footer/Footer.jsx
+++ src/js/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-	return <h4>world mundo</h4>
+	return (
+		<nav className="navbar bg-dark mt-5 p-4">
+			<div className="container-fluid justify-content-center">
+				<span className="navbar-text text-light">
+				Copyright © Flavia's Landing Page 2023
+				</span>
+			</div>
+		</nav>
+	)
 };
 
 export default Footer;
